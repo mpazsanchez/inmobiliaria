fb8cb69e7303cdc57c3738b4d7e72b011154d284
Maria Paz Sanchez <maria_16s@hotmail.com>
2025-07-31 12:02:11 -0300

    feat: agregar componente de barra de navegación pública y estilos de Bootstrap

---
+++ src/app/layout/public-layout/components/public-navbar/public-navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';  
+
+@Component({
+  selector: 'app-public-navbar',
+  standalone: true,
+  imports: [NgbModule],
+  templateUrl: './public-navbar.component.html',
+  styleUrl: './public-navbar.component.scss'
+})
+export class PublicNavbarComponent {
+
+}
