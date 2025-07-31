068bbbba0b38da61b1243680ec71b55a6317ac46
Maria Paz Sanchez <maria_16s@hotmail.com>
2025-07-31 16:21:39 -0300

    feat: agregar hero section con video de fondo y estilos correspondientes Agregar favicon

--- src/app/features/public-site/pages/home/home.component.ts
+++ src/app/features/public-site/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "../../components/home/hero-section/hero-section.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
