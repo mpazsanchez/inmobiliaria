08331726ef5d694adc74a7415db17847053d7e00
Maria Paz Sanchez <maria_16s@hotmail.com>
2025-08-04 16:09:55 -0300

    feat: agregar detección de plataforma en componentes AboutCompany y PublicNavbar para evitar errores relacionados al ssr

--- src/app/features/public-site/components/home/about-company/about-company.component.ts
+++ src/app/features/public-site/components/home/about-company/about-company.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 interface CompanyStats {
   value: number;
@@ -20,6 +20,11 @@ interface SkillProgress {
   styleUrl: './about-company.component.scss'
 })
 export class AboutCompanyComponent {
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   // Estadísticas de la empresa
   companyStats: CompanyStats[] = [
@@ -73,6 +78,8 @@ export class AboutCompanyComponent {
   }
 
   onCallPhone(): void {
+    if (!this.isBrowser) return;
+    
     window.open(`tel:${this.contactInfo.phone}`, '_self');
   }
 
