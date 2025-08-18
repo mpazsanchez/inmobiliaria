49db37c5c068ad8f4ed9fc4523bb3081dceb0af9
Maria Paz Sanchez <maria_16s@hotmail.com>
2025-08-18 14:52:48 -0300

    feat: actualizar estilos y estructura de componentes de testimonios y características de productos

--- src/app/features/public-site/components/products/product-features/product-features.component.ts
+++ src/app/features/public-site/components/products/product-features/product-features.component.ts
@@ -10,38 +10,7 @@ import { ProductFeature } from '../../../models';
   selector: 'app-product-features',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <section class="product-features py-5">
-      <div class="container">
-        
-        @if (title) {
-          <div class="row mb-5">
-            <div class="col-12 text-center">
-              <h2 class="section-title">{{ title }}</h2>
-              @if (subtitle) {
-                <p class="section-subtitle">{{ subtitle }}</p>
-              }
-            </div>
-          </div>
-        }
-        
-        <div class="row g-4">
-          @for (feature of features; track $index) {
-            <div class="col-lg-3 col-md-6 col-sm-12">
-              <div class="feature-card h-100">
-                <div class="feature-icon">
-                  <i [class]="feature.icon"></i>
-                </div>
-                <h4 class="feature-title">{{ feature.title }}</h4>
-                <p class="feature-description">{{ feature.description }}</p>
-              </div>
-            </div>
-          }
-        </div>
-        
-      </div>
-    </section>
-  `,
+  template: './product-features.component.html',
   styleUrl: './product-features.component.scss'
 })
 export class ProductFeaturesComponent {
