5a66f5801d5e62a832ab9eb5a9d9af3e6ad3320b
Maria Paz Sanchez <maria_16s@hotmail.com>
2025-08-20 10:47:22 -0300

    feat: agregar componente de diseño y barra lateral para el área de miembros

---
+++ src/app/layout/member-layout/member-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-layout',
+  standalone: true,
+  templateUrl: './member-layout.component.html',
+  styleUrls: ['./member-layout.component.scss']
+})
+export class MemberLayoutComponent {}
