55b3d0c4aed3cdb18f67b205d3c5c42bed8f41bb
Maria Paz Sanchez <maria_16s@hotmail.com>
2026-01-16 08:56:28 -0300

    feat: agregar componente PropertySkeleton con su HTML, SCSS y lógica

---
+++ src/app/features/public-site/components/property-skeleton/property-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-property-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './property-skeleton.component.html',
+  styleUrl: './property-skeleton.component.scss'
+})
+export class PropertySkeletonComponent {}
