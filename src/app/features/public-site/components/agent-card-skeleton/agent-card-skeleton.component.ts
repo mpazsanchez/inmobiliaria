eed753194970c45275dec01003b551e265f1ea01
Maria Paz Sanchez <maria_16s@hotmail.com>
2026-01-23 12:20:48 -0300

    feat: agregar componente de esqueleto de tarjeta de agente con estilos y estructura HTML

---
+++ src/app/features/public-site/components/agent-card-skeleton/agent-card-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-agent-card-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './agent-card-skeleton.component.html',
+  styleUrl: './agent-card-skeleton.component.scss'
+})
+export class AgentCardSkeletonComponent {}
