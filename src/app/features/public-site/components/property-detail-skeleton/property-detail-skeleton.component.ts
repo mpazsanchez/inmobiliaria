902556bbdb3be83023d8974e8147167bdce87182
Maria Paz Sanchez <maria_16s@hotmail.com>
2026-01-23 12:34:19 -0300

    feat: agregar componente de esqueleto de detalle de propiedad con estilos y estructura

---
+++ src/app/features/public-site/components/property-detail-skeleton/property-detail-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-property-detail-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './property-detail-skeleton.component.html',
+  styleUrl: './property-detail-skeleton.component.scss'
+})
+export class PropertyDetailSkeletonComponent {}
