228f9682b545f5378fa7af11b2d9cd2c38357fb2
Maria Paz Sanchez <maria_16s@hotmail.com>
2026-01-30 15:53:25 -0300

    feat: agregar interceptor de errores al cliente HTTP en la configuración de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([errorInterceptor])
+    ),
+    provideAnimations()
   ]
 };
