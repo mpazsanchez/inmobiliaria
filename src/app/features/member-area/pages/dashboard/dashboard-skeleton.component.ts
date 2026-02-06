b75fa25b7980778bda6a7c769ca141615c8cc77a
Maria Paz Sanchez <maria_16s@hotmail.com>
2026-02-06 16:48:35 -0300

    feat: agregar componente de esqueleto para el dashboard con estilos y estructura HTML

---
+++ src/app/features/member-area/pages/dashboard/dashboard-skeleton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-skeleton.component.html',
+  styleUrl: './dashboard-skeleton.component.scss'
+})
+export class DashboardSkeletonComponent {}
